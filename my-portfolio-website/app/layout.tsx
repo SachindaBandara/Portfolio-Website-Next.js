5840f49fdef8c14c85a73ab127e8c32d71c093ab
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-26 20:43:14 +0530

    add

--- my-portfolio-website/app/layout.tsx
+++ my-portfolio-website/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}
-      bg-gray-50 text-gray-950 relative`}>
+      bg-gray-50 text-gray-950 relative `}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        
+        <Header />
         {children}
         </body>
     </html>
