e2e72d5d920a1d9c02ad9521313c5dc01914dc22
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-27 22:20:26 +0530

    add

--- my-portfolio-website/components/header.tsx
+++ my-portfolio-website/components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
+      <div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
     </header>
   );
 }
