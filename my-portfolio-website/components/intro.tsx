67303d879180ce3fca9ce20fd04b30328632941a
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-28 21:31:58 +0530

    add

--- my-portfolio-website/components/intro.tsx
+++ my-portfolio-website/components/intro.tsx
@@ -4,7 +4,12 @@ import React from "react";
 import Image from "next/image";
 import Profile from "@/public/profile pic.jpg";
 import { delay, motion } from "framer-motion";
-import { BsArrowRight, BsBehance, BsDribbble, BsLinkedin } from "react-icons/bs";
+import {
+  BsArrowRight,
+  BsBehance,
+  BsDribbble,
+  BsLinkedin,
+} from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import Link from "next/link";
 import { FaGithubSquare } from "react-icons/fa";
@@ -54,28 +59,46 @@ export default function intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-          delay: 0.1
+          delay: 0.1,
         }}
       >
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
-          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a href="" download className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5">
-          Download CV <HiDownload  className="opacity-60 group-hover:translate-y-1 transition"/>
+        <a
+          href=""
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-        <a href="https://www.linkedin.com/in/sachinda-bandara-2a9344248/" className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none ocus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5">
+        <a
+          href="https://www.linkedin.com/in/sachinda-bandara-2a9344248/"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none ocus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5"
+        >
           <BsLinkedin />
         </a>
-        <a href="https://github.com/SachindaBandara" className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5">
+        <a
+          href="https://github.com/SachindaBandara"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5"
+        >
           <FaGithubSquare />
         </a>
-        <a href="https://www.behance.net/sachindubandar/projects" className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5">
+        <a
+          href="https://www.behance.net/sachindubandar/projects"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5"
+        >
           <BsBehance />
         </a>
-        <a href="https://dribbble.com/Sachinda_Bandara" className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none ocus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5">
+        <a
+          href="https://dribbble.com/Sachinda_Bandara"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none ocus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/5"
+        >
           <BsDribbble />
         </a>
       </motion.div>
