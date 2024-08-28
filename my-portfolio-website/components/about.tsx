97e4c7044de75510d37ccb62cbfbaa75ab21f281
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-28 23:49:59 +0530

    create project section

--- my-portfolio-website/components/about.tsx
+++ my-portfolio-website/components/about.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import SectionHeading from './section-heading'
+"use client"
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { delay, motion } from "framer-motion";
 
 export default function About() {
   return (
-    <section className='mb-28 max-w-[45rem] text-center leading-7 sm:mb-40'>
-        <SectionHeading>About me</SectionHeading>
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-7 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.175,
+      }}
+    >
+      <SectionHeading>About me</SectionHeading>
 
-        <p className='mb-3'>
-        I'm passionate about building exceptional user experiences by weaving together the power of frontend development. I'm constantly expanding my knowledge of UI & UX, with the goal of creating seamless interfaces that are both functional and aesthetically pleasing.
-        </p>
+      <p className="mb-3">
+        I'm passionate about building exceptional user experiences by weaving
+        together the power of frontend development. I'm constantly expanding my
+        knowledge of UI & UX, with the goal of creating seamless interfaces that
+        are both functional and aesthetically pleasing.
+      </p>
 
-        <p>
-        I thrive on innovation and am always eager to learn about the latest advancements in technology and design trends.
-        </p>
-
-    </section>
-  )
+      <p>
+        I thrive on innovation and am always eager to learn about the latest
+        advancements in technology and design trends.
+      </p>
+    </motion.section>
+  );
 }
---
+++ my-portfolio-website/components/projects.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return (
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8">
+      <h3 className="text-2xl font-semibold">{title}</h3>
+      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+      <ul className="flex flex-wrap mt-4 gap-2">
+        {tags.map((tag, index) => (
+          <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} alt="Projects" quality={95} />
+    </section>
+  );
+}
