c3f513d9e0c32ed2f5b6647181f4890010c10383
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-30 22:13:37 +0530

    add the scroll animations to the project cards

--- my-portfolio-website/components/section-heading.tsx
+++ my-portfolio-website/components/section-heading.tsx
@@ -5,5 +5,5 @@ type SectionHeadingProps = {
 };
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
-  return <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>;
+  return <h2 className="text-3xl font-bold capitalize mb-8 text-center">{children}</h2>;
 }
