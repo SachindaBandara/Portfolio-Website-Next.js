b57920df13ac681527a301bc8ad185923a523f79
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-30 20:45:10 +0530

    add project card animation

--- my-portfolio-website/components/section-heading.tsx
+++ my-portfolio-website/components/section-heading.tsx
@@ -5,5 +5,5 @@ type SectionHeadingProps = {
 };
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+  return <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>;
 }
