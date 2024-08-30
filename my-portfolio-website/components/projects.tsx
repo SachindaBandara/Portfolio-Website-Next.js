1501d7d04ecf204d46340810673e746a939217a5
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-30 20:45:20 +0530

    add

--- my-portfolio-website/components/projects.tsx
+++ my-portfolio-website/components/projects.tsx
@@ -24,8 +24,8 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[20rem]">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-lg">
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -44,8 +44,17 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
         src={imageUrl}
         alt="Projects"
         quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        group-even:right-[initial] group-even:-left-40 "
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+        transition
+        group-hover:scale-[1.04] 
+        group-hover:-translate-x-3
+        group-hover:translate-y-3 
+        group-hover:-rotate-2
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3 
+        group-even:group-hover:rotate-2
+        group-even:right-[initial] 
+        group-even:-left-40 "
       />
     </section>
   );
