ce4ec6215cab264fb9a59b53fbc6269fb26951ae
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-30 20:34:50 +0530

    add

--- my-portfolio-website/components/projects.tsx
+++ my-portfolio-website/components/projects.tsx
@@ -24,8 +24,8 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]">
-      <div className="pt-4 pb-9 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0">
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[20rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -44,7 +44,8 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
         src={imageUrl}
         alt="Projects"
         quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        group-even:right-[initial] group-even:-left-40 "
       />
     </section>
   );
