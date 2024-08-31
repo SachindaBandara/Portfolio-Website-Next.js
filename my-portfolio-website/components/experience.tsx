4f44f4f3450ac47537211299db31682e1b293f01
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-31 22:32:43 +0530

    add

--- my-portfolio-website/components/experience.tsx
+++ my-portfolio-website/components/experience.tsx
@@ -13,30 +13,32 @@ export default function Experience() {
   return (
     <section id="experience">
       <SectionHeading>My Education</SectionHeading>
-      <VerticalTimeline>
+      <VerticalTimeline lineColor=" ">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
-            contentStyle={{
-              background:"#f3f4f6",
-              boxShadow: "none",
-              border: "1px solid rgbs(0,0,0,0.05)",
-              textAlign: "left",
-              padding: "1.3rem 2rem",
-            }}
-            contentArrowStyle={{
-              borderRight: "0.4rem solid #9ca3af",
-            }}
-            date={item.date}
-            icon={item.icon}
-            iconStyle={{
-              background: "white",
-              fontSize: "1.5rem",
-            }}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgbs(0,0,0,0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
             >
-              <h3>{item.title}</h3>
-              <p>{item.location}</p>
-              <p>{item.description}</p>
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">
+                {item.description}
+              </p>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
