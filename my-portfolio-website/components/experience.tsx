538a63f5a99370c7ff778c6aef388bd25a241f37
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-01 00:31:23 +0530

    add

--- my-portfolio-website/components/experience.tsx
+++ my-portfolio-website/components/experience.tsx
@@ -17,7 +17,7 @@ export default function Experience() {
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
-      <SectionHeading>My experience</SectionHeading>
+      <SectionHeading>My Education</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
