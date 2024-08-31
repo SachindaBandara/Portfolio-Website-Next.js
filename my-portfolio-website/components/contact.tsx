4f4d1f6c817568c4c5cc4f9f6b56e2302b55a994
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-31 23:41:11 +0530

    add

--- my-portfolio-website/components/contact.tsx
+++ my-portfolio-website/components/contact.tsx
@@ -4,14 +4,23 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Contact() {
-    const { ref } = useSectionInView("Contact");
+  const { ref } = useSectionInView("Contact");
 
   return (
-    <section ref={ref} id="contact" className="mb-28 w-[min(100%,38rem)] scroll-mt-28 sm:mb-40">
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="mb-28 w-[min(100%,38rem)] text-center scroll-mt-28 sm:mb-40"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{duration: 1}}
+      viewport={{ once: true,}}
+    >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className="text-gray-700">
+      <p className="text-gray-700 -mt-4">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:jmsachindabandara@gmail.com">
           jmsachindabandara@gmail.com
@@ -19,16 +28,33 @@ export default function Contact() {
         or through this form.
       </p>
 
-      <form className="mt-10">
+      <form className="mt-10 flex flex-col">
         <input
-          className="h-14 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack"
           type="email"
+          placeholder="Your email"
+          required
+          maxLength={500}
         />
-        <textarea className="h-52 my-3 rounded-lg borderBlack"/>
-        <button type="submit">
-          Submit <FaPaperPlane />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+        >
+          Submit{" "}
+          <FaPaperPlane
+            className="text-xs opacity-70 transition-all 
+          group-hover:translate-x-1 
+          group-hover:translate-y-1
+          focus:scale-110 hover:scale-110  hover:bg-gray-950 active:scale-105"
+          />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
