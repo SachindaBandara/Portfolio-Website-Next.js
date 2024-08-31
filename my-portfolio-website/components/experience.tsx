8f39dfd641dac2da3f64606583d1f758223820ed
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-01 00:21:52 +0530

    add

--- my-portfolio-website/components/experience.tsx
+++ my-portfolio-website/components/experience.tsx
@@ -36,8 +36,7 @@ export default function Experience() {
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                // background:
-                //   theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                background: "white",
                 fontSize: "1.5rem",
               }}
             >
