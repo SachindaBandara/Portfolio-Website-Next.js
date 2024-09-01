b5564e109dfd48941011c0f01d3a3eba4c9a73cc
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-01 23:10:53 +0530

    add

--- my-portfolio-website/components/education.tsx
+++ my-portfolio-website/components/education.tsx
@@ -20,35 +20,38 @@ export default function Education() {
       className="scroll-mt-28 mb-28 sm:mb-40 relative" /* Added 'relative' position */
     >
       <SectionHeading>My Education</SectionHeading>
-      <VerticalTimeline lineColor="#9ca3af">
+      <VerticalTimeline lineColor="">
         {educationData.map((item, index) => (
+          <React.Fragment key={index}>
           <VerticalTimelineElement
-            key={index}
+            visible={true}
             contentStyle={{
               background: "#f3f4f6",
               boxShadow: "none",
               border: "1px solid rgba(0, 0, 0, 0.05)",
               textAlign: "left",
-              padding: "1.3rem 2rem",
+              padding: "1.5rem 2rem",
             }}
             contentArrowStyle={{
-              borderRight: "0.4rem solid #9ca3af",
+              borderRight: "0.6rem solid #ffffff",
             }}
             date={item.date}
             icon={item.icon}
             iconStyle={{
-              background: "white",
+              background: "black",
               fontSize: "1.5rem",
             }}
           >
-            <h3 className="font-semibold capitalize">{item.title}</h3>
-            <p className="font-normal !mt-0">{item.location}</p>
-            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+            <h3 className="font-semibold capitalize text-gray-950">{item.title}</h3>
+            <p className="font-normal  text-gray-950 !mt-0">{item.location}</p>
+            <p className="!mt-1 !font-normal  text-gray-800 ">
               {item.description}
             </p>
           </VerticalTimelineElement>
+          </React.Fragment>
         ))}
       </VerticalTimeline>
+      
     </section>
   );
 }
