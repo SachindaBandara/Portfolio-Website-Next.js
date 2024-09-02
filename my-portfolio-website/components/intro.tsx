d672244a6316184fd2b40b979d59bfafb7c7270f
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-02 23:09:41 +0530

    add

--- my-portfolio-website/components/intro.tsx
+++ my-portfolio-website/components/intro.tsx
@@ -96,7 +96,7 @@ export default function intro() {
         <a
           href=""
           download
-          className="group bg-gray-800 text-gray-100 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5 hover:text-white"
+          className="group bg-gray-100 text-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/5 hover:text-gray-950"
         >
           Download CV{" "}
           <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
