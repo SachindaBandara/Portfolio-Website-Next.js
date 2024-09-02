628bad1acbd1bffdec750755cede211f700c4b15
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-02 21:37:03 +0530

    add

--- my-portfolio-website/components/footer.jsx
+++ my-portfolio-website/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='mb-10 px-4 text-center text-gray-400'>
-        <small className='mb-2 block text-xs'>
+        <small className='mt-4 mb-2 block text-xs'>
            Copyright &copy; 2024 Sachinda Bandara. All Rights Reserved.
         </small>
     </footer>
