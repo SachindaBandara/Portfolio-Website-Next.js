760311b4cef7403ddb024bda211e20f10ef34a73
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-05 23:21:18 +0530

    add

--- my-portfolio-website/context/active-section-context.tsx
+++ my-portfolio-website/context/active-section-context.tsx
@@ -1,49 +1,47 @@
 "use client";
 
-import type{ SectionName } from "@/lib/types";
+import type { SectionName } from "@/lib/types";
 import React, { useState, createContext, useContext } from "react";
-
-
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
 };
 
 type ActiveSectionContextType = {
-    activeSection: SectionName;
-    setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
-    timeOfLastClick: number,
-    setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
-}
-export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
-  const [timeOfLastClick, setTimeOfLastClick]  = useState(0);
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
 
   return (
-  <ActiveSectionContext.Provider value={{
-    activeSection,
-    setActiveSection,
-    timeOfLastClick,
-    setTimeOfLastClick,
-  }}>
-    {children}
-  </ActiveSectionContext.Provider>
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
   );
 }
 
-export function useActiveSectionContext(){
-    const context = useContext(ActiveSectionContext);
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
 
-    if(context === null){
-        throw new Error(
-            "Error Message!"
-        );
-    }
+  if (context === null) {
+    throw new Error("Error Message!");
+  }
 
-    return context;
-
+  return context;
 }
