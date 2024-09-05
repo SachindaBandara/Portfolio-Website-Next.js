725f41305984f5f23fd4ed0b61f3600b83a316be
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-05 23:19:49 +0530

    add

--- my-portfolio-website/components/submit-btn.tsx
+++ my-portfolio-website/components/submit-btn.tsx
@@ -9,7 +9,8 @@ export default function SubmitBtn() {
     <button
       type="submit"
       className="group flex items-center justify-center mt-3 border-2 border-amber-0 gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110  hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65"
-    disabled = {pending}>
+      disabled={pending}
+    >
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
       ) : (
