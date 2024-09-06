c645d25c167321469cacc6816ff52b829a2957a1
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-06 21:25:00 +0530

    add

--- my-portfolio-website/app/layout.tsx
+++ my-portfolio-website/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className}
-      bg-slate-950 text-slate-50 relative h-[5000px] pt-28 sm:pt-36 `}
+      bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36 `}
       >
-        <div className="bg-[#fbe2e300] absolute top-[-6rem] -z-10 right-[20rem] h-[20rem] w-[22rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb00] absolute top-[-1rem] -z-10 left-[-35rem] h-[20rem] w-[22rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[15rem] h-[20rem] w-[22rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[15rem] h-[20rem] w-[22rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
         <ActiveSectionContextProvider>
           <Header />
--- my-portfolio-website/components/project.tsx
+++ my-portfolio-website/components/project.tsx
@@ -33,10 +33,10 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition rounded-lg group-even:pl-9">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
-          <h3 className="text-3xl font-extrabold text-gray-950">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+      <section className=" bg-gray-900 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-slate-100 transition rounded-lg group-even:pl-9">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col  h-full group-even:ml-[18rem]">
+          <h3 className="text-3xl font-extrabold text-slate-50 ">{title}</h3>
+          <p className="mt-2 leading-relaxed text-slate-700">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
