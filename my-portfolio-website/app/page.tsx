a40012b8e4d980dc90b7a1858c38e27dbf304f3b
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-24 23:09:04 +0530

    add

--- my-portfolio-website/app/page.tsx
+++ my-portfolio-website/app/page.tsx
@@ -6,6 +6,7 @@ import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
@@ -13,8 +14,8 @@ export default function Home() {
       <SectionDivider />
       <About />
       <SectionDivider />
-      <Education />
-      <SectionDivider />
+      {/* <Education />
+      <SectionDivider /> */}
       <Projects />
       <SectionDivider />
       <Skills />
